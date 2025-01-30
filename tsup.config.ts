4d74b7716760734861f81f9cd6ffe8bf030990a6
Avaer Kazmer <ultimus@gmail.com>
2025-01-29 20:27:20 -0800

    Update tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
         "form-data",
         "axios",
         "agentkeepalive",
+        "@elizaos/core",
         // Add other modules you want to externalize
     ],
 });
